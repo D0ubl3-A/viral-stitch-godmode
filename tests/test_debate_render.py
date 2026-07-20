57c745fdb64323f3407956070243d04096a18a19
D0ubl3-A <D0ubl3a0@gmail.com>
2026-07-20 15:54:54 -0700

    Make renderer timestamp test stable

--- tests/test_debate_render.py
+++ tests/test_debate_render.py
@@ -8,7 +8,9 @@ def test_clip_segments_rebases_word_timestamps():
         {"start": 10.8, "end": 11.2, "text": "that."},
     ]
     segments = _clip_segments(words, 10.0, 12.0)
-    assert segments == [{"start": 0.0, "end": 1.1999999999999993, "text": "You said that."}]
+    assert segments[0]["start"] == 0.0
+    assert round(segments[0]["end"], 3) == 1.2
+    assert segments[0]["text"] == "You said that."
 
 
 def test_render_srt_time():
